86cb7ca66a92755704cdc5a4c24934f5c8902542
oded282 <45912772+oded282@users.noreply.github.com>
2019-09-07 16:28:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import init_centroids
 import k_means
 import loader
 import sys
+import plot
 
 RGB_SIZE = 3
 IMG_SIZE = 128
@@ -15,16 +15,12 @@ THIRD_ARG = 3
 
 def main():
     img = loader.load_data(sys.argv[FIRST_ARG])
-
-    for i in range(1, 5):
-        centroids = init_centroids.init_centroids(np.power(2, i))
+    for power in range(1, 5):
+        centroids = init_centroids.init_centroids(np.power(2, power))
         model = k_means.KMeans(centroids, img)
         new_img = model.algorithm(EPOCH)
         new_img = np.reshape(new_img, (int(sys.argv[SEC_ARG]), int(sys.argv[THIRD_ARG]), RGB_SIZE))
-        # plot the image
-        plt.imshow(new_img)
-        plt.grid(False)
-        plt.show()
+        plot.plot(new_img)
 
 
 if __name__ == "__main__":
